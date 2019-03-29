23310523811b9fe6a6da835c4ec6e4c11c37a8fa
Andrei Ciurea <aciurea@outlook.com>
2019-03-29 11:36:08 +0200

    Added repository to controllers.

--- CityInfo.API/Controllers/CitiesController.cs
+++ CityInfo.API/Controllers/CitiesController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using CityInfo.API.Models;
+using CityInfo.API.Services;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CityInfo.API.Controllers
@@ -10,18 +11,39 @@ namespace CityInfo.API.Controllers
     [Route("api/cities")]
     public class CitiesController : Controller
     {
+        private ICityInfoRepository _repository;
+
+        public CitiesController(ICityInfoRepository repository)
+        {
+            _repository = repository;
+        }
+
         [HttpGet()]
         public IActionResult GetCities()
         {
             // an empty list of cities is a valid response: there are no cities
-            return Ok(CitiesDataStore.Current.Cities);
+            var cities = _repository.GetCities();
+
+            var results = new List<CityWithoutPOIsDto>();
+
+            foreach (var cityEntity in cities)
+            {
+                results.Add(new CityWithoutPOIsDto
+                {
+                    Id = cityEntity.Id,
+                    Name = cityEntity.Name,
+                    Description = cityEntity.Description
+                });
+            }
+
+            return Ok(results);
         }
 
         [HttpGet("{id}")]
         public IActionResult GetCity(int id)
         {
             //find city
-            var city = CitiesDataStore.Current.Cities.FirstOrDefault(p => p.Id == id);
+            var city = _repository.GetCity(id, false);
             if (city == null)
                 return NotFound();
 
